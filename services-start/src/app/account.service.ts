b7e65fbcdafa98918570d0c57745cee8af5c073e
Michael Rinkovski <mri@unik.dk>
2021-12-10 13:58:46 +0100

    Added service assignment

--- services-start/src/app/account.service.ts
+++ services-start/src/app/account.service.ts
@@ -1,3 +1,8 @@
+import { Injectable, EventEmitter } from "@angular/core";
+import { LoggingService } from "./logging.service";
+
+@Injectable()
+
 export class AccountsService {
     accounts = [
         {
@@ -13,11 +18,18 @@ export class AccountsService {
           status: 'unknown'
         }
       ];
+
+    statusUpdated = new EventEmitter<string>();
+
+    constructor(private loggingService: LoggingService) {}
+
     addAccount(name: string, status: string) {
         this.accounts.push({name: name, status: status});
+        this.loggingService.logStatusChange(status);
     }
 
     updateStatus(id: number, status: string) {
         this.accounts[id].status = status;
+        this.loggingService.logStatusChange(status);
     }
 }
